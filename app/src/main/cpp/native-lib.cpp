9562e23ac400e2e10c5185b5b559b6e1c7f807df
yetote <503779938@qq.com>
2019-03-13 18:18:31 +0800

    手快，点错了

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,13 +3,7 @@
 #include "decode/Decode.h"
 
 Decode decode;
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_myplayer_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_myplayer_player_MyPlayer_prepare(JNIEnv *env, jobject instance, jstring path_) {
